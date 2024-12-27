5db8f7c2da46cfe9bbc891a7ac515ee6940bc506
Matt <157317090+pan-kroczek@users.noreply.github.com>
2024-12-27 17:29:25 +0100

    Customizable etch-a-sketch area

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const container = document.querySelector("#container");
+let side = 16;
+let area = side * side;
+let percent = 100 / side;
 
-for (i = 0; i < 256; i++) {
+for (i = 0; i < area; i++) {
     let square = document.createElement("div");
     square.classList.add("square");
+    square.style.flexBasis = `${percent}%`;
     container.appendChild(square);
 }
